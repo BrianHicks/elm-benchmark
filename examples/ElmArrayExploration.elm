a9a4b59839c48bef65af8197d6a360161cfbf62d
Brian Hicks <brian@brianthicks.com>
2017-02-22 16:47:11 -0600

    examples(ElmArrayExploration): reorder benchmarks

--- examples/ElmArrayExploration.elm
+++ examples/ElmArrayExploration.elm
@@ -1,7 +1,7 @@
 module ElmArrayExploration exposing (..)
 
-import Array.Hamt as Array exposing (Array)
 import Array as NArray
+import Array.Hamt as Array exposing (Array)
 import Benchmark exposing (..)
 import Benchmark.Runner exposing (BenchmarkProgram, program)
 
@@ -23,51 +23,24 @@ suite n =
 
         cSampleArray =
             NArray.initialize n identity
-
-        sampleList =
-            List.range 1 n
-
-        equalButDifferentOperation =
-            Array.initialize n identity
     in
         describe
             ("Array (" ++ toString n ++ " elements)")
             [ Benchmark.compare "initialize"
-                (benchmark2 "HAMT" Array.initialize n identity)
                 (benchmark2 "Core" NArray.initialize n identity)
+                (benchmark2 "HAMT" Array.initialize n identity)
             , Benchmark.compare "set"
-                (benchmark3 "HAMT" Array.set 7 5 sampleArray)
                 (benchmark3 "Core" NArray.set 7 5 cSampleArray)
+                (benchmark3 "HAMT" Array.set 7 5 sampleArray)
             , Benchmark.compare "push"
-                (benchmark2 "HAMT" Array.push 5 sampleArray)
                 (benchmark2 "Core" NArray.push 5 cSampleArray)
+                (benchmark2 "HAMT" Array.push 5 sampleArray)
             , Benchmark.compare "get"
-                (benchmark2 "HAMT" Array.get 5 sampleArray)
                 (benchmark2 "Core" NArray.get 5 cSampleArray)
+                (benchmark2 "HAMT" Array.get 5 sampleArray)
             , Benchmark.compare "append"
-                (benchmark2 "HAMT" Array.append sampleArray sampleArray)
                 (benchmark2 "Core" NArray.append cSampleArray cSampleArray)
-              -- , describe "Slice"
-              --     [ benchmark3 "from beginning minor" Array.slice 3 n sampleArray
-              --     , benchmark3 "from end minor" Array.slice 0 -3 sampleArray
-              --     , benchmark3 "from end major" Array.slice 0 (n // 2) sampleArray
-              --     , benchmark3 "from both minor" Array.slice 3 -3 sampleArray
-              --     , benchmark3 "from both major" Array.slice ((n // 2) - 10) (n // 2) sampleArray
-              --     ]
-              -- , benchmark3 "Foldl" Array.foldl (\_ acc -> acc + 1) 0 sampleArray
-              -- , benchmark3 "Foldr" Array.foldr (\_ acc -> acc + 1) 0 sampleArray
-              -- , benchmark2 "Map" Array.map identity sampleArray
-              -- , benchmark2 "Filter" Array.filter (always True) sampleArray
-              -- , benchmark2 "Indexed Map" Array.indexedMap (,) sampleArray
-              -- , benchmark1 "From List" Array.fromList sampleList
-              -- , benchmark1 "To List" Array.toList sampleArray
-              -- , benchmark1 "Indexed List" Array.toIndexedList sampleArray
-              -- , benchmark1 "toString" Array.toString sampleArray
-              -- , describe "Equality"
-              --     [ benchmark2 "success" (==) sampleArray (Array.set 5 5 sampleArray)
-              --     , benchmark2 "failure" (==) sampleArray (Array.set 5 7 sampleArray)
-              --     , benchmark2 "worst case" (==) sampleArray equalButDifferentOperation
-              --     ]
+                (benchmark2 "HAMT" Array.append sampleArray sampleArray)
             ]
 
 
