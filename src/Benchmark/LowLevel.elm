bb6038a9544d3a073a9d41b936b7bcf0143c4db4
Brian Hicks <brian@brianthicks.com>
2017-02-23 14:09:12 -0600

    Benchmark.Lowlevel: improve docs

--- src/Benchmark/LowLevel.elm
+++ src/Benchmark/LowLevel.elm
@@ -16,7 +16,14 @@ module Benchmark.LowLevel
 
 {-| Low Level Elm Benchmarking API
 
-This API exposes the raw tasks necessary to create higher-level benchmarking abstractions.
+This API exposes the raw tasks necessary to create higher-level benchmarking
+abstractions.
+
+As a user, you're probably not going to need to use this library. Take a look at
+`Benchmark` instead, it has the user-friendly primitives. If you *do* find
+yourself using this library often, please [open an issue on
+`elm-benchmark`](https://github.com/BrianHicks/elm-benchmark/issues/new) and
+we'll find a way to make your use case friendlier.
 
 # Operations
 @docs Operation, operation, operation1, operation2,  operation3, operation4, operation5, operation6, operation7, operation8
@@ -42,6 +49,15 @@ single function call.
 
 Create these using `operation` through `operation8` and take runtime samples
 using `sample`.
+
+**Note:** Small samples of `operation` through `operation8` produce results that
+are close enough to be deceiving. Across large enough sample sizes, comparing
+operations created with different functions will result in larger and larger
+skews. Prefer `operation1` through `operation8` if you can (they're easier to
+use) but if in doubt, stick everything in `operation`. Benchmark speed is not an
+*absolute* measure, but a *relative* one. Make sure that you get your relations
+right.
+
 -}
 type Operation
     = Operation
@@ -131,10 +147,14 @@ operation8 =
 {-| Run an operation a specified number of times. The returned value is the
 total time it took for the given number of runs.
 
-High-resolution timing data from these functions comes from the [Performance
-API](https://developer.mozilla.org/en-US/docs/Web/API/Performance). If
-`performance.now` is unavailble, it will fall back to
-[Date](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date).
+In the browser, high-resolution timing data from these functions comes from the
+[Performance API](https://developer.mozilla.org/en-US/docs/Web/API/Performance)
+and is accurate to 5µs. If `performance.now` is unavailable, it will fall back
+to [Date](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date),
+accurate to 1ms.
+
+In alternative runners, consult the runner documentation for resolution
+information.
 -}
 sample : Int -> Operation -> Task Error Time
 sample =
