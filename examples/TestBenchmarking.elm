cb6386bea64c365cd6c26e1d6a589ff121e04f28
Brian Hicks <brian@brianthicks.com>
2017-02-23 16:37:31 -0600

    examples(TestBenchmarking): use actual thunks

--- examples/TestBenchmarking.elm
+++ examples/TestBenchmarking.elm
@@ -33,28 +33,28 @@ arities =
                 h
          in
             [ Benchmark.compare "1"
-                (Benchmark.benchmark "benchmark" fn1)
+                (Benchmark.benchmark "benchmark" <| \_ -> fn1 ())
                 (Benchmark.benchmark1 "benchmark1" fn1 ())
             , Benchmark.compare "2"
-                (Benchmark.benchmark "benchmark" <| fn2 ())
+                (Benchmark.benchmark "benchmark" <| \_ -> fn2 () ())
                 (Benchmark.benchmark2 "benchmark2" fn2 () ())
             , Benchmark.compare "3"
-                (Benchmark.benchmark "benchmark" <| fn3 () ())
+                (Benchmark.benchmark "benchmark" <| \_ -> fn3 () () ())
                 (Benchmark.benchmark3 "benchmark3" fn3 () () ())
             , Benchmark.compare "4"
-                (Benchmark.benchmark "benchmark" <| fn4 () () ())
+                (Benchmark.benchmark "benchmark" <| \_ -> fn4 () () () ())
                 (Benchmark.benchmark4 "benchmark4" fn4 () () () ())
             , Benchmark.compare "5"
-                (Benchmark.benchmark "benchmark" <| fn5 () () () ())
+                (Benchmark.benchmark "benchmark" <| \_ -> fn5 () () () () ())
                 (Benchmark.benchmark5 "benchmark5" fn5 () () () () ())
-            , Benchmark.compare "7"
-                (Benchmark.benchmark "benchmark" <| fn6 () () () () ())
+            , Benchmark.compare "6"
+                (Benchmark.benchmark "benchmark" <| \_ -> fn6 () () () () () ())
                 (Benchmark.benchmark6 "benchmark6" fn6 () () () () () ())
-            , Benchmark.compare "8"
-                (Benchmark.benchmark "benchmark" <| fn7 () () () () () ())
+            , Benchmark.compare "7"
+                (Benchmark.benchmark "benchmark" <| \_ -> fn7 () () () () () () ())
                 (Benchmark.benchmark7 "benchmark7" fn7 () () () () () () ())
-            , Benchmark.compare "9"
-                (Benchmark.benchmark "benchmark" <| fn8 () () () () () () ())
+            , Benchmark.compare "8"
+                (Benchmark.benchmark "benchmark" <| \_ -> fn8 () () () () () () () ())
                 (Benchmark.benchmark8 "benchmark8" fn8 () () () () () () () ())
             ]
         )
