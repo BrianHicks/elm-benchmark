07cef3adb611744363297da233cf00f1e6cfbe31
Brian Hicks <brian@brianthicks.com>
2017-12-15 16:27:44 -0600

    rip out old reporting approach

--- src/Benchmark/Reporting.elm
+++ src/Benchmark/Reporting.elm
@@ -1,139 +1,29 @@
 module Benchmark.Reporting
     exposing
         ( Report(..)
-        , Stats
-        , compareMeanRuntime
-        , compareOperationsPerSecond
         , fromBenchmark
-        , meanRuntime
-        , operationsPerSecond
-        , stats
-        , totalOperations
-        , totalRuntime
         )
 
 {-| Reporting for Benchmarks
 
-@docs Report, Stats, stats
-
-@docs fromBenchmark
-
-
-# Analysis
-
-@docs totalOperations, totalRuntime
-
-@docs meanRuntime, compareMeanRuntime
-
-@docs operationsPerSecond, compareOperationsPerSecond
+@docs Report, fromBenchmark
 
 -}
 
 import Benchmark.Benchmark as Benchmark exposing (Benchmark)
 import Benchmark.Status as Status exposing (Status(..))
-import Time exposing (Time)
 
 
 {-| Reports are the public version of Benchmarks.
 
-Each tag of Report has a name and some other information about the structure of
-a benchmarking run.
+Each tag of Report has a name and some other information about the
+structure of a benchmarking run.
 
 -}
 type Report
     = Single String Status
     | Series String (List ( String, Status ))
     | Group String (List Report)
-
-
-{-| Stats returned from a successful benchmarking run
--}
-type alias Stats =
-    { sampleSize : Int
-    , samples : List Time
-    }
-
-
-{-| Calculate stats from a sample size and total runtime
--}
-stats : Int -> List Time -> Stats
-stats =
-    Stats
-
-
-mean : List Float -> Float
-mean numbers =
-    List.sum numbers / toFloat (List.length numbers)
-
-
-stddev : List Float -> Float
-stddev numbers =
-    let
-        thisMean =
-            mean numbers
-    in
-    numbers
-        |> List.map (\n -> (n - thisMean) ^ 2)
-        |> mean
-        |> sqrt
-
-
-{-| total number of samples
--}
-totalOperations : Stats -> Int
-totalOperations stats =
-    stats.sampleSize * List.length stats.samples
-
-
-{-| total runtime
--}
-totalRuntime : Stats -> Time
-totalRuntime stats =
-    List.sum stats.samples
-
-
-{-| Calculate mean runtime. The returned value is `(runtime, stddev)`
--}
-meanRuntime : Stats -> ( Time, Time )
-meanRuntime stats =
-    let
-        meanStddev =
-            stats.samples
-                |> List.map (\sample -> sample / toFloat stats.sampleSize)
-                |> stddev
-
-        mean =
-            totalRuntime stats / toFloat (totalOperations stats)
-    in
-    ( mean, meanStddev )
-
-
-{-| Compare mean runtimes, given as a percentage difference of the first to the
-second
--}
-compareMeanRuntime : Stats -> Stats -> Float
-compareMeanRuntime a b =
-    (meanRuntime a |> Tuple.first) / (meanRuntime b |> Tuple.first) - 1
-
-
-{-| Calculate operations per second. The returned value is `(meanOpsPerSec, stddev)`
--}
-operationsPerSecond : Stats -> ( Float, Float )
-operationsPerSecond stats =
-    let
-        opsPerSec =
-            stats.samples
-                |> List.map (\sample -> toFloat stats.sampleSize * (Time.second / sample))
-    in
-    ( mean opsPerSec, stddev opsPerSec )
-
-
-{-| Compare operations per second, given as a percentage difference of the first
-to the second
--}
-compareOperationsPerSecond : Stats -> Stats -> Float
-compareOperationsPerSecond a b =
-    (operationsPerSecond a |> Tuple.first) / (operationsPerSecond b |> Tuple.first) - 1
 
 
 
--- tests/Benchmark/ReportingTest.elm
+++ tests/Benchmark/ReportingTest.elm
@@ -71,98 +71,15 @@ report =
 
 
 -- now, finally: the tests
-
-
-totalOperations : Test
-totalOperations =
-    describe "totalOperations"
-        [ test "with one sample and sample size one" <|
-            \() ->
-                Reporting.stats 1 [ 1 ]
-                    |> Reporting.totalOperations
-                    |> Expect.equal 1
-        , fuzz (Fuzz.intRange 1 10000) "with many samples" <|
-            \size ->
-                size
-                    |> List.range 1
-                    |> List.map toFloat
-                    |> Reporting.stats 1
-                    |> Reporting.totalOperations
-                    |> Expect.equal size
-        , fuzz (Fuzz.intRange 1 (10 ^ 9)) "with many sample sizes" <|
-            \size ->
-                size
-                    |> flip Reporting.stats [ 1 ]
-                    |> Reporting.totalOperations
-                    |> Expect.equal size
-        ]
-
-
-totalRuntime : Test
-totalRuntime =
-    describe "totalRuntime"
-        [ test "with one sample and sample size one" <|
-            \() ->
-                Reporting.stats 1 [ 1 ]
-                    |> Reporting.totalOperations
-                    |> Expect.equal 1
-        , fuzz (Fuzz.list Fuzz.float) "with many samples" <|
-            \samples ->
-                samples
-                    |> Reporting.stats 1
-                    |> Reporting.totalRuntime
-                    |> Expect.equal (List.sum samples)
-        ]
-
-
-meanRuntime : Test
-meanRuntime =
-    describe "meanRuntime"
-        [ test "with one sample and sample size one" <|
-            \() ->
-                Reporting.stats 1 [ 1 ]
-                    |> Reporting.meanRuntime
-                    |> Expect.equal ( 1, 0 )
-        , test "with one large and one small sample" <|
-            \() ->
-                Reporting.stats 1 [ 0, 2 ]
-                    |> Reporting.meanRuntime
-                    |> Expect.equal ( 1, 1 )
-        ]
-
-
-operationsPerSecond : Test
-operationsPerSecond =
-    describe "operationsPerSecond"
-        [ test "with one sample and sample size one" <|
-            \() ->
-                Reporting.stats 1 [ Time.second ]
-                    |> Reporting.operationsPerSecond
-                    |> Expect.equal ( 1, 0 )
-        , test "when an operation takes longer than a second" <|
-            \() ->
-                Reporting.stats 1 [ 2 * Time.second ]
-                    |> Reporting.operationsPerSecond
-                    |> Expect.equal ( 0.5, 0 )
-        , fuzz (Fuzz.intRange 1 (1 ^ 6 * 5)) "fit into one second" <|
-            \operations ->
-                operations
-                    |> flip Reporting.stats [ Time.second ]
-                    |> Reporting.operationsPerSecond
-                    |> Expect.equal ( toFloat operations, 0 )
-        , fuzz (Fuzz.intRange 1 40) "one operation per second for n seconds" <|
-            \operations ->
-                operations
-                    |> List.range 1
-                    |> List.map (always Time.second)
-                    |> Reporting.stats 1
-                    |> Reporting.operationsPerSecond
-                    |> Expect.equal ( 1, 0 )
-        ]
-
-
-
 -- TODO: We're definitely going to want these decoders tested, when they get rewritten.
+
+
+dummy : Test
+dummy =
+    test "always success so elm-test doesn't complain for now" (\_ -> Expect.pass)
+
+
+
 -- serialization : Test
 -- serialization =
 --     describe "serialization"
