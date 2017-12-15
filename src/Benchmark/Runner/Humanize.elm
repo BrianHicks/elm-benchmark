a9d6f8cc385cc68361b1fcdb8563f1efb7a34920
Brian Hicks <brian@brianthicks.com>
2017-12-15 13:08:32 -0600

    humanize integers, so 1000000 becomes 1,000,000

--- src/Benchmark/Runner/Humanize.elm
+++ src/Benchmark/Runner/Humanize.elm
@@ -4,6 +4,22 @@ module Benchmark.Runner.Humanize exposing (int, percent)
 int : Int -> String
 int =
     toString
+        >> String.toList
+        >> List.reverse
+        >> groupsOf 3
+        >> List.reverse
+        >> List.map (List.reverse >> String.fromList)
+        >> String.join ","
+
+
+groupsOf : Int -> List a -> List (List a)
+groupsOf howMany items =
+    case List.take howMany items of
+        [] ->
+            []
+
+        xs ->
+            xs :: groupsOf howMany (List.drop howMany items)
 
 
 percent : Float -> String
